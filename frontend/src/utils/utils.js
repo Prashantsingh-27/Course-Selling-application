f52879f2b514e9cc32af2f88b56459c618f0f29d
Prashantsingh-27 <prashantsinghdemon27@gmail.com>
2025-05-03 14:18:44 +0530

    backend link updated

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,1 +1,2 @@
-export const BACKEND_URL="http://localhost:4002/api/v1";
+// export const BACKEND_URL="http://localhost:4002/api/v1";
+export const BACKEND_URL="https://course-selling-application-6xam.onrender.com/api/v1";
